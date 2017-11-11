bc3b7c3078d81281e4ba472342611877b1c103ec
Michal Granec <michal.granec@innovatric.com>
2017-11-11 09:18:36 +0100

    add Landing view, Search container and first components

---
+++ src/components/containers/SearchContainer/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import SearchBox from "../../SearchBox";
+
+class SearchContainer extends Component {
+
+    render() {
+        return (
+            <SearchBox />
+        );
+    }
+}
+
+export default SearchContainer;
