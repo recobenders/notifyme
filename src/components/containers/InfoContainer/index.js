b5e4ad72f03d408ff21c7961934ea2534e077fd3
Michal Granec <michal.granec@innovatric.com>
2017-11-11 11:59:11 +0100

    update item InfoContainer

--- src/components/containers/InfoContainer/index.js
+++ src/components/containers/InfoContainer/index.js
@@ -3,9 +3,25 @@ import React, { Component } from 'react';
 class InfoContainer extends Component {
 
     render() {
+        const item = this.props.item;
+        console.log(item);
         return (
             <div>
-                Some basic item info details
+                <p>
+                    Name: {item.name}
+                </p>
+                <p>
+                    Type: {item['@type'].join(', c')}
+                </p>
+                <p>
+                    Description: {item.description}
+                </p>
+                <p>
+                    Detailed Description: {item.detailedDescription.articleBody}
+                </p>
+                <p>
+                    Link: {item.url}
+                </p>
             </div>
         );
     }
