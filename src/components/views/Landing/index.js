7e9c3938ba0ce7cc91ee43e7429fc00aeea9b9b9
Michal Granec <michal.granec@innovatric.com>
2017-11-11 13:41:01 +0100

    add functional ItemList view

--- src/components/views/Landing/index.js
+++ src/components/views/Landing/index.js
@@ -3,11 +3,19 @@ import SearchContainer from "../../containers/SearchContainer";
 
 class Landing extends Component {
 
+    handleSearchSubmit = (items) => {
+        this.props.history.push({
+                pathname: "/item-list",
+                state: {items: items}
+            }
+        );
+    };
+
     render() {
         return (
             <div>
                 <h2>Landing view</h2>
-                <SearchContainer />
+                <SearchContainer handleSearchSubmit={this.handleSearchSubmit} />
             </div>
         );
     }
