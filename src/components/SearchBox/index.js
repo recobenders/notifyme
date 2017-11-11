0e83981cbacdb3bf1c134a52bcfbd842a86cec50
Michal Granec <michal.granec@innovatric.com>
2017-11-11 18:35:18 +0100

    redesign landing page

--- src/components/SearchBox/index.js
+++ src/components/SearchBox/index.js
@@ -1,20 +1,45 @@
 import React, { Component } from 'react';
-import Select from 'react-select';
 import 'react-select/dist/react-select.css';
 import { withStyles } from 'material-ui/styles';
 import Button from 'material-ui/Button';
+import Grid from 'material-ui/Grid';
+import TextField from 'material-ui/TextField';
+import Search from 'material-ui-icons/Search';
 
 const styles = theme => ({
     button: {
         margin: theme.spacing.unit,
-    }
+    },
+    root: {
+        flexGrow: 0,
+    },
+    leftIcon: {
+        marginRight: theme.spacing.unit,
+    },
 });
 
 const Knowledge = require('knowledge-node')({ serverKey: process.env.REACT_APP_KNOWLEDGE_API_KEY });
 
 class SearchBox extends Component {
+    constructor(props) {
+        super(props);
+        this.timeout = null;
+    }
 
-    getOptions = (input) => {
+    searchKnowledgeApi = (event) => {
+        clearTimeout(this.timeout);
+
+        let searchTerm = event.target.value;
+        this.timeout = setTimeout(() => {
+            if(searchTerm !== ''){
+                this.knowledgeSearch(searchTerm);
+            } else {
+                this.props.handleResultUpdate(null);
+            }
+        }, 200);
+    };
+
+    knowledgeSearch = (input) => {
         const types = [
             Knowledge.types.movies,
             Knowledge.types.musicAlbum,
@@ -34,10 +59,6 @@ class SearchBox extends Component {
         return Knowledge.search(params)
             .then(body => {
                 this.props.handleResultUpdate(body.itemListElement);
-                // return { options: body.itemListElement.map((item) => ({
-                //     label: item.result.name,
-                //     value: item.result.name
-                // })) };
             })
             .catch(error => {
                 return { options: [] };
@@ -48,18 +69,27 @@ class SearchBox extends Component {
         const { classes } = this.props;
 
         return (
-            <div>
-                <div >
-                    Submit and get to ItemList
-                </div>
-                <Select.Async
-                    name="search_box"
-                    loadOptions={this.getOptions}
-                />
-                <Button raised className={classes.button} onClick={this.props.handleResultSubmit}>
-                    Default
-                </Button>
-            </div>
+            <Grid container className={classes.root} >
+                <Grid item xs={12}>
+                    <Grid container justify="center" spacing={8}>
+                        <Grid item xs={8} style={{flexGrow: 0}}>
+                            <TextField
+                                type="search"
+                                placeholder="Search for movie, TV show, video game or music album you are interested in"
+                                margin="normal"
+                                fullWidth
+                                onChange={this.searchKnowledgeApi}
+                            />
+                        </Grid>
+                        <Grid item xs={2} style={{flexGrow: 0}}>
+                            <Button raised className={classes.button} onClick={this.props.handleResultSubmit}>
+                                <Search className={classes.leftIcon} />
+                                Show me more
+                            </Button>
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Grid>
         );
     }
 }
