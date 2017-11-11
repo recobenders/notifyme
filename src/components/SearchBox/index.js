c1988f5e78b39c93be97d58d77b0f3fdfe56e470
Michal Granec <michal.granec@innovatric.com>
2017-11-11 10:40:04 +0100

    add simple Knowledge graph autocomplete

--- src/components/SearchBox/index.js
+++ src/components/SearchBox/index.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
+
+const Knowledge = require('knowledge-node')({ serverKey: process.env.REACT_APP_KNOWLEDGE_API_KEY });
 
 class SearchBox extends Component {
+
+    getOptions = (input) => {
+        const types = [
+            Knowledge.types.thing
+        ];
+        const limit = 10;
+        let params;
+
+        try {
+            params = Knowledge.buildParams(input, types, limit);
+        } catch(e) {
+            return Promise.resolve({ options: [] });
+        }
+
+        return Knowledge.search(params)
+            .then(body => {
+                this.props.handleResultUpdate(body.itemListElement);
+                return { options: body.itemListElement.map((item) => ({
+                    label: item.result.name,
+                    value: item.result.name
+                })) };
+            })
+            .catch(error => {
+                return { options: [] };
+            });
+    };
 
     render() {
         return (
             <div>
-                Search Box
+                <Select.Async
+                    name="search_box"
+                    loadOptions={this.getOptions}
+                />
             </div>
         );
     }
