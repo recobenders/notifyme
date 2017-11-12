ed0487879e697a2072da0dacf642aedba8e606f1
jvisnovsky <1135003+juraj-visnovsky@users.noreply.github.com>
2017-11-12 08:36:36 +0100

    Redesign item container with material ui cards

--- src/components/containers/NotifyContainer/index.js
+++ src/components/containers/NotifyContainer/index.js
@@ -6,17 +6,36 @@ import Typography from 'material-ui/Typography';
 class NotifyContainer extends Component {
 
     render() {
+        const futureEvents = this.props.releaseDates.filter(releaseDate =>
+            releaseDate >= new Date()
+        );
+
+        const pastEvents = this.props.releaseDates.filter(releaseDate =>
+            releaseDate < new Date()
+        );
+
+        let futureEventsLabel = null;
+        if(futureEvents.length > 0) {
+            futureEventsLabel = <Grid item xs={12}>
+                <Typography
+                    align={'center'} >
+                    {this.props.item.name} is about to be published on:
+                </Typography>
+            </Grid>;
+        } else {
+            futureEventsLabel = <Grid item xs={12}>
+                <Typography
+                    align={'center'} >
+                    No upcoming release dates were found for this item.
+                </Typography>
+            </Grid>;
+        }
+
         return (
             <div>
                 <Grid container spacing={16} justify="center" >
-                    <Grid item xs={12}>
-                        <Typography
-                            align={'center'} >
-                            {this.props.item.name} is about to be released on:
-                        </Typography>
-                    </Grid>
-
-                    {this.props.releaseDates.map((releaseDate, i) =>
+                    {futureEventsLabel}
+                    {futureEvents.map((releaseDate, i) =>
                         <Grid item xs={12} key={i}>
                             <Grid container spacing={16}>
                                 <Grid item xs={6}>
