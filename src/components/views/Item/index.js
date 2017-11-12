661e0905c24e44c25a2df433901820da533f8ec8
jvisnovsky <1135003+juraj-visnovsky@users.noreply.github.com>
2017-11-12 11:51:15 +0100

    Add publication location

--- src/components/views/Item/index.js
+++ src/components/views/Item/index.js
@@ -4,7 +4,6 @@ import wdk from 'wikidata-sdk';
 import InfoContainer from "../../containers/InfoContainer";
 import NotifyContainer from "../../containers/NotifyContainer";
 import Grid from 'material-ui/Grid';
-import Typography from 'material-ui/Typography';
 import { withStyles } from 'material-ui/styles';
 
 const styles = theme => ({
@@ -18,24 +17,41 @@ class Item extends Component {
         super(props);
         this.state = {
             item: props.location.state.item,
-            releaseDates: []
+            releaseDates: {}
         };
     }
 
     // DRY
     componentDidMount() {
         const sparql = `
-SELECT DISTINCT ?date WHERE {
+SELECT DISTINCT ?date ?imdbId ?placeOfPublicationLabel WHERE {
   ?item ?label "${this.state.item.name}"@en .
   ?item wdt:P577 ?date .
+  OPTIONAL { ?item wdt:P345 ?imdbId. }
+  ?statement ps:P577 ?date.
+  ?statement pq:P291 ?placeOfPublication.
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
 } ORDER BY ASC(?date)
-LIMIT 10
 `;
         const url = wdk.sparqlQuery(sparql);
 
         axios.get(url)
             .then(res => {
-                const releaseDates = res.data.results.bindings.map(item => new Date(item.date.value));
+                const items = res.data.results.bindings;
+                let releaseDates = {};
+                for (let item of items) {
+                    const date = new Date(item.date.value);
+                    const location = item.placeOfPublicationLabel.value;
+                    if(date in releaseDates) {
+                        releaseDates[date].locations.push(location);
+                    } else {
+                        releaseDates[date] = {
+                            date: date,
+                            locations: [location]
+                        };
+                    }
+                }
+
                 this.setState({ releaseDates: releaseDates });
             });
     }
