52a9dc265fcfca2f2323fda00ba37ea75cb9e432
Michal Granec <michal.granec@innovatric.com>
2017-11-12 23:01:14 +0100

    add feature icons to Landing screen

--- src/components/views/Landing/index.js
+++ src/components/views/Landing/index.js
@@ -4,6 +4,12 @@ import Typography from 'material-ui/Typography';
 import Grid from 'material-ui/Grid';
 import Divider from 'material-ui/Divider';
 import { withStyles } from 'material-ui/styles';
+import MovieIcon from 'material-ui-icons/Movie'
+import MusicIcon from 'material-ui-icons/LibraryMusic';
+import TVIcon from 'material-ui-icons/Tv';
+import GameIcon from 'material-ui-icons/VideogameAsset';
+import Avatar from 'material-ui/Avatar';
+import { deepOrange, grey } from 'material-ui/colors';
 
 const styles = theme => ({
     root: {
@@ -21,6 +27,24 @@ const styles = theme => ({
     logo: {
         width: 180,
         height: 180
+    },
+    twoLine: {
+        display: 'flex',
+        flexDirection: 'column',
+    },
+    avatar: {
+        width: 100,
+        height: 100,
+        marginLeft: 50,
+        backgroundColor: deepOrange[300]
+    },
+    svgIcon: {
+        width: 60,
+        height: 60,
+        color: grey[800],
+    },
+    iconText: {
+        marginTop: 20
     }
 });
 
@@ -68,6 +92,38 @@ class Landing extends Component {
                             buttonText={'More'}
                             hideResultsAfterSubmit={false}
                         />
+                    </Grid>
+                    <Grid item xs={12} style={{marginTop: 70}}>
+                        <Grid container justify='center'>
+                            <Grid item xs={8}>
+                                <Grid container style={{textAlign: 'center'}}>
+                                    <Grid item xs={3} className={classes.twoLine}>
+                                        <Avatar className={classes.avatar}>
+                                            <MovieIcon className={classes.svgIcon}/>
+                                        </Avatar>
+                                        <Typography className={classes.iconText} type='subheading'>Movies</Typography>
+                                    </Grid>
+                                    <Grid item xs={3} className={classes.twoLine}>
+                                        <Avatar className={classes.avatar}>
+                                            <TVIcon className={classes.svgIcon}/>
+                                        </Avatar>
+                                        <Typography className={classes.iconText} type='subheading'>TV Shows</Typography>
+                                    </Grid>
+                                    <Grid item xs={3} className={classes.twoLine}>
+                                        <Avatar className={classes.avatar}>
+                                            <GameIcon className={classes.svgIcon}/>
+                                        </Avatar>
+                                        <Typography className={classes.iconText} type='subheading'>Video Games</Typography>
+                                    </Grid>
+                                    <Grid item xs={3} className={classes.twoLine}>
+                                        <Avatar className={classes.avatar}>
+                                            <MusicIcon className={classes.svgIcon}/>
+                                        </Avatar>
+                                        <Typography className={classes.iconText} type='subheading'>Music Albums</Typography>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Grid>
                     </Grid>
                 </Grid>
             </div>
